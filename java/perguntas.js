097fb48236a9c62ef09368905f250decaa2c750f
jtomacheski12 <140642624+jtomacheski12@users.noreply.github.com>
2025-11-03 09:50:43 -0300

    rthjhkj

---
+++ java/perguntas.js
@@ -0,0 +1,16 @@
+export const pergunstas = [
+    enunciado"Em uma comunidade rural fictícia chamada Vale Verde, a quase totalidade das 500 famílias (cerca de 2.000 pessoas) utiliza a lenha e o carvão, provenientes de manejo não sustentável de florestas vizinhas, como única fonte de energia para cozinhar",
+"Um novo projeto governamental visa eliminar totalmente o uso da lenha e do carvão na comunidade em um prazo de cinco anos, substituindo-os por alternativas mais limpas, como gás liquefeito de petróleo (GLP), biogás ou energia solar térmica para cocção.O que você faria ?";
+
+alternativas:{
+    texto: 
+"A comunidade rural Vale Verde é altamente dependente da lenha e do carvão para suas necessidades energéticas diárias. Recentemente, foi anunciado um plano governamental ambicioso para proibir o uso desses combustíveis em um prazo de dois anos, substituindo-os por alternativas mais limpas. Essa mudança visa combater o desmatamento e melhorar a saúde respiratória da população.
+
+"Apesar dos benefícios ambientais e de saúde evidentes, a transição gera um intenso debate na comunidade, pois afeta profundamente o modo de vida e a economia local.
+    afirmacao:[
+           A afirmação que justifica a Alternativa 1 (Aceleração e Adaptação) é:
+            "Acelerar a transição energética é imperativo, pois os benefícios diretos para a saúde pública e a conservação ambiental, como a redução imediata da poluição do ar interno e o fim do desmatamento não sustentável, superam o risco logístico e financeiro de curto prazo, desde que o governo garanta a implementação de um robusto e imediato programa de subsídios e capacitação."
+       ],
+   }
+
+]
