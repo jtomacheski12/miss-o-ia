a7b1bd3f0e78e7c8d9dea22b7595fb975c741a20
jtomacheski12 <140642624+jtomacheski12@users.noreply.github.com>
2025-11-05 14:16:23 -0300

    fdgh

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,16 +1,21 @@
 export const pergunstas = [
     enunciado"Em uma comunidade rural fictícia chamada Vale Verde, a quase totalidade das 500 famílias (cerca de 2.000 pessoas) utiliza a lenha e o carvão, provenientes de manejo não sustentável de florestas vizinhas, como única fonte de energia para cozinhar",
 "Um novo projeto governamental visa eliminar totalmente o uso da lenha e do carvão na comunidade em um prazo de cinco anos, substituindo-os por alternativas mais limpas, como gás liquefeito de petróleo (GLP), biogás ou energia solar térmica para cocção.O que você faria ?";
+],
 
 alternativas:{
-    texto: 
-"A comunidade rural Vale Verde é altamente dependente da lenha e do carvão para suas necessidades energéticas diárias. Recentemente, foi anunciado um plano governamental ambicioso para proibir o uso desses combustíveis em um prazo de dois anos, substituindo-os por alternativas mais limpas. Essa mudança visa combater o desmatamento e melhorar a saúde respiratória da população.
-
-"Apesar dos benefícios ambientais e de saúde evidentes, a transição gera um intenso debate na comunidade, pois afeta profundamente o modo de vida e a economia local.
+    texto: "Adotar integralmente e acelerar o plano de transição energética. A comunidade foca na organização imediata para a instalação de biodigestores comunitários (produção de biogás) e na formação de cooperativas para a compra subsidiada de fogões a gás e painéis solares.";
     afirmacao:[
-           A afirmação que justifica a Alternativa 1 (Aceleração e Adaptação) é:
             "Acelerar a transição energética é imperativo, pois os benefícios diretos para a saúde pública e a conservação ambiental, como a redução imediata da poluição do ar interno e o fim do desmatamento não sustentável, superam o risco logístico e financeiro de curto prazo, desde que o governo garanta a implementação de um robusto e imediato programa de subsídios e capacitação."
        ],
+       proxima: 1,
+   },
+   {
+    texto: "Resistir ao prazo de dois anos e negociar com o governo um período de transição estendido (ex: cinco a dez anos), buscando subsídios de longo prazo e treinamento técnico antes de qualquer proibição. A comunidade argumenta que a lenha/carvão é a única energia gratuita e culturalmente aceita.";
+   afirmacao:[
+    "A Resistência é vital para proteger as famílias mais pobres de Vale Verde do choque financeiro e da insegurança energética de uma proibição imediata. Negociar um prazo maior garante a adaptação gradual e o desenvolvimento de subsídios permanentes, evitando que a transição para energia limpa gere pobreza e exclusão social."
+   ],
+   proxima: 2,
    }
 
-]
+
